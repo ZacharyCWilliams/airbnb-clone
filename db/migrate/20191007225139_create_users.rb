cbcd3e51787d6d247317a3d5eed6a05d8cfef50f
zach <zachcwilliams@icloud.com>
2019-10-07 16:46:04 -0700

    authentication up to sessions#destroy

---
+++ db/migrate/20191007225139_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :email, null: false, unique: true
+      t.string :name, null: false
+      t.string :password_digest, null: false
+      t.string :session_token, null: false
+      t.integer :listing_id
+
+      t.timestamps
+    end
+    add_index :users, :name
+  end
+end
